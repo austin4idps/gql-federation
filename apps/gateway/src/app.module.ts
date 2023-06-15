e29777a339ccbbeb95039b7a65eb39bc1c6addcd
austin4idps <austin@4idps.com>
2023-06-15 17:04:45 +0800

    fix(app): remove get-hello

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,11 +1,9 @@
 import { IntrospectAndCompose } from '@apollo/gateway';
 import { DbModule } from '@app/db';
-import { Member } from '@app/db/entities/src/member.entity';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -15,7 +13,6 @@ import { AppService } from './app.service';
       isGlobal: true,
     }),
     DbModule,
-    TypeOrmModule.forFeature([Member]),
     GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
       gateway: {
--- apps/gateway/src/app.service.ts
+++ apps/gateway/src/app.service.ts
@@ -1,15 +1,6 @@
-import { Member } from '@app/db/entities/src/member.entity';
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  constructor(
-    @InjectRepository(Member)
-    private memberRepo: Repository<Member>
-  ){}
-  async getHello()  {
-    return await this.memberRepo.find()
-  }
+  constructor() {}
 }
