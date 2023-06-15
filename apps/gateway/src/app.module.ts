4e7613ebf3fe1a4a167417730755f87a23dc213c
austin4idps <austin@4idps.com>
2023-06-15 15:08:27 +0800

    chore(app): add sub-graph config

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,7 +1,10 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
 import { DbModule } from '@app/db';
 import { Member } from '@app/db/entities/src/member.entity';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,10 +14,26 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-  DbModule,
-  TypeOrmModule.forFeature([Member])
-],
+    DbModule,
+    TypeOrmModule.forFeature([Member]),
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'member',
+              url: 'http://localhost:3001/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+function ApolloServerPluginInlineTraceDisabled() {
+  throw new Error('Function not implemented.');
+}
